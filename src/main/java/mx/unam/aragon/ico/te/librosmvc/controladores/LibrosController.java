10fe75e70c022c808c761db3cb60433c3ab08916
Jazmoonc <jazminmonse1231@outlook.es>
2025-05-18 17:48:52 -0600

    Diseño de dar alta nuevo libro

--- src/main/java/mx/unam/aragon/ico/te/librosmvc/controladores/LibrosController.java
+++ src/main/java/mx/unam/aragon/ico/te/librosmvc/controladores/LibrosController.java
@@ -1,10 +1,10 @@
 package mx.unam.aragon.ico.te.librosmvc.controladores;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import mx.unam.aragon.ico.te.librosmvc.modelos.Libro;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/libreria")
@@ -19,7 +19,23 @@ public class LibrosController {
     public String libro(Model model) {
         Libro libro = new Libro("Boulevard", "Flor M. Salvador","Wattpad","https://is1-ssl.mzstatic.com/image/thumb/Publication126/v4/35/a5/0e/35a50e2e-db0e-3de9-130a-3f934ce8f3fa/9788419169884.jpg/1200x1200wz.jpg",309);
         model.addAttribute("libro",libro);
-
         return "libro";
     }
+
+    @GetMapping("/formulario")
+    public String nuevo(Model model) {
+        model.addAttribute("libro", new Libro("Todo lo que fuimos","Alberto Villarreal","Planeta","img",213));
+        return "formLibro";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(
+            @ModelAttribute Libro libro
+            ) {
+        //Falta terminar
+        LoggerFactory.getLogger(getClass()).info("Guardando libro + " + libro);
+
+        return "redirect:/libreria/formulario?exito";
+    }
+
 }
